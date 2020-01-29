020ef153e959af8a4bcf898d33238f02a67e8f8f
virajt71 <soulr27@gmail.com>
2020-01-30 00:11:04 +0100

    Update Project_6.py

--- Project_6.py
+++ Project_6.py
@@ -1,6 +1,4 @@
-Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 23:09:28) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> class_names = ['wheel', 'pizza', 'smiley_face', 'apple', 'potato', 'basketball', 'soccer_ball', 'brain', 'clock', 'circle']
+class_names = ['wheel', 'pizza', 'smiley_face', 'apple', 'potato', 'basketball', 'soccer_ball', 'brain', 'clock', 'circle']
 
 import urllib.request
 import os 
